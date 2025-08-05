081d0157f5bd339d3104af1b34f6869e97584523
Jeff <jeff.retz08@gmail.com>
2025-08-05 14:29:13 +0200

    fix font rendering

--- composeApp/src/jsMain/kotlin/dev/datlag/mimasu/ui/theme/Font.kt
+++ composeApp/src/jsMain/kotlin/dev/datlag/mimasu/ui/theme/Font.kt
@@ -9,12 +9,20 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import dev.datlag.mimasu.composeapp.generated.resources.Manrope_VariableFont_wght
+import androidx.compose.ui.text.font.GenericFontFamily
+import dev.datlag.mimasu.composeapp.generated.resources.Manrope_Bold
+import dev.datlag.mimasu.composeapp.generated.resources.Manrope_ExtraBold
+import dev.datlag.mimasu.composeapp.generated.resources.Manrope_ExtraLight
+import dev.datlag.mimasu.composeapp.generated.resources.Manrope_Light
+import dev.datlag.mimasu.composeapp.generated.resources.Manrope_Medium
+import dev.datlag.mimasu.composeapp.generated.resources.Manrope_Regular
+import dev.datlag.mimasu.composeapp.generated.resources.Manrope_SemiBold
 import dev.datlag.mimasu.composeapp.generated.resources.NotoSansJP_VariableFont_wght
 import dev.datlag.mimasu.composeapp.generated.resources.NotoSansKR_VariableFont_wght
 import dev.datlag.mimasu.composeapp.generated.resources.NotoSansSC_VariableFont_wght
 import dev.datlag.mimasu.composeapp.generated.resources.Res
 import kotlinx.atomicfu.atomic
+import kotlinx.serialization.Serializable
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.FontResource
 import org.jetbrains.compose.resources.preloadFont
@@ -30,7 +38,7 @@ data object Font {
     @Composable
     fun manrope(): FontFamily {
         if (manropeFamily == null) {
-            manropeFamily = loadFontFamily(Res.font.Manrope_VariableFont_wght)
+            manropeFamily = loadFontFamily(Type.Manrope)
         }
         val fallbackFont = loadCJKFallback()
 
@@ -40,24 +48,22 @@ data object Font {
     @Composable
     fun loadCJKFallback(): FontFamily? {
         if (jpFamily == null) {
-            jpFamily = loadFontFamily(Res.font.NotoSansJP_VariableFont_wght)
+            jpFamily = loadFontFamily(Type.NotoJP)
         }
         if (krFamily == null) {
-            krFamily = loadFontFamily(Res.font.NotoSansKR_VariableFont_wght)
+            krFamily = loadFontFamily(Type.NotoKR)
         }
         if (scFamily == null) {
-            scFamily = loadFontFamily(Res.font.NotoSansSC_VariableFont_wght)
+            scFamily = loadFontFamily(Type.NotoSC)
         }
 
         return scFamily ?: jpFamily ?: krFamily
     }
 
     @Composable
-    private fun loadFontFamily(
-        resource: FontResource
-    ): FontFamily? {
+    private fun loadFontFamily(type: Type): FontFamily? {
         val fontFamilyResolver = LocalFontFamilyResolver.current
-        val fonts = loadVariableFont(resource)
+        val fonts = loadVariableFont(type)
         return if (fonts.isEmpty()) {
             null
         } else {
@@ -73,21 +79,21 @@ data object Font {
 
     @OptIn(ExperimentalResourceApi::class)
     @Composable
-    private fun loadVariableFont(resource: FontResource): List<Font> {
-        val extraLight by preloadFont(resource, FontWeight.ExtraLight)
-        val extraLightItalic by preloadFont(resource, FontWeight.ExtraLight, FontStyle.Italic)
-        val light by preloadFont(resource, FontWeight.Light)
-        val lightItalic by preloadFont(resource, FontWeight.Light, FontStyle.Italic)
-        val regular by preloadFont(resource, FontWeight.Normal)
-        val regularItalic by preloadFont(resource, FontWeight.Normal, FontStyle.Italic)
-        val medium by preloadFont(resource, FontWeight.Medium)
-        val mediumItalic by preloadFont(resource, FontWeight.Medium, FontStyle.Italic)
-        val semiBold by preloadFont(resource, FontWeight.SemiBold)
-        val semiBoldItalic by preloadFont(resource, FontWeight.SemiBold, FontStyle.Italic)
-        val bold by preloadFont(resource, FontWeight.Bold)
-        val boldItalic by preloadFont(resource, FontWeight.Bold, FontStyle.Italic)
-        val extraBold by preloadFont(resource, FontWeight.ExtraBold)
-        val extraBoldItalic by preloadFont(resource, FontWeight.ExtraBold, FontStyle.Italic)
+    private fun loadVariableFont(type: Type): List<Font> {
+        val extraLight by preloadFont(type.extraLight, FontWeight.ExtraLight)
+        val extraLightItalic by preloadFont(type.extraLight, FontWeight.ExtraLight, FontStyle.Italic)
+        val light by preloadFont(type.light, FontWeight.Light)
+        val lightItalic by preloadFont(type.light, FontWeight.Light, FontStyle.Italic)
+        val regular by preloadFont(type.normal, FontWeight.Normal)
+        val regularItalic by preloadFont(type.normal, FontWeight.Normal, FontStyle.Italic)
+        val medium by preloadFont(type.medium, FontWeight.Medium)
+        val mediumItalic by preloadFont(type.medium, FontWeight.Medium, FontStyle.Italic)
+        val semiBold by preloadFont(type.semiBold, FontWeight.SemiBold)
+        val semiBoldItalic by preloadFont(type.semiBold, FontWeight.SemiBold, FontStyle.Italic)
+        val bold by preloadFont(type.bold, FontWeight.Bold)
+        val boldItalic by preloadFont(type.bold, FontWeight.Bold, FontStyle.Italic)
+        val extraBold by preloadFont(type.extraBold, FontWeight.ExtraBold)
+        val extraBoldItalic by preloadFont(type.extraBold, FontWeight.ExtraBold, FontStyle.Italic)
 
         return remember(
             extraLight, extraLightItalic,
@@ -107,6 +113,56 @@ data object Font {
                 bold, boldItalic,
                 extraBold, extraBoldItalic
             )
+        }
+    }
+
+    @Serializable
+    sealed interface Type {
+
+        val extraLight: FontResource
+            get() = light
+
+        val light: FontResource
+            get() = normal
+
+        val normal: FontResource
+
+        val medium: FontResource
+            get() = normal
+
+        val semiBold: FontResource
+            get() = medium
+
+        val bold: FontResource
+            get() = semiBold
+
+        val extraBold: FontResource
+            get() = bold
+
+        @Serializable
+        data object Manrope : Type {
+            override val extraLight: FontResource = Res.font.Manrope_ExtraLight
+            override val light: FontResource = Res.font.Manrope_Light
+            override val normal: FontResource = Res.font.Manrope_Regular
+            override val medium: FontResource = Res.font.Manrope_Medium
+            override val semiBold: FontResource = Res.font.Manrope_SemiBold
+            override val bold: FontResource = Res.font.Manrope_Bold
+            override val extraBold: FontResource = Res.font.Manrope_ExtraBold
+        }
+
+        @Serializable
+        data object NotoJP : Type {
+            override val normal: FontResource = Res.font.NotoSansJP_VariableFont_wght
+        }
+
+        @Serializable
+        data object NotoKR : Type {
+            override val normal: FontResource = Res.font.NotoSansKR_VariableFont_wght
+        }
+
+        @Serializable
+        data object NotoSC : Type {
+            override val normal: FontResource = Res.font.NotoSansSC_VariableFont_wght
         }
     }
 }
